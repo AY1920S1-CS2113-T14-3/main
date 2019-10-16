128753e86af88cd2cee83aabada72e28ad6d72d3
chelsea148629 <chelsea148629@gmail.com>
2019-10-16 12:20:41 +0800

    Revert "Implement HistoryCommand and fix delete bugs."

--- src/main/java/exception/WrongHistoryFormatException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class WrongHistoryFormatException extends WrongFormatException {
-    public WrongHistoryFormatException() {
-        super("     ☹ OOPS: Expected format \"history {int value}\"");
-    }
-}
